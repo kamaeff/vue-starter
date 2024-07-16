f3e54943cb5097bc87de1a9f65f1094295ef62f9
anton <kamaeff2@gmail.com>
2024-07-16 18:46:21 +0300

    fix

--- src/components/header/HeaderTemplate.vue
+++ src/components/header/HeaderTemplate.vue
@@ -3,7 +3,5 @@
 </script>
 
 <template>
-  <div class="header">
-    <p>Header works</p>
-  </div>
+  <div class="header"></div>
 </template>
--- src/components/main/MainTemplate.vue
+++ src/components/main/MainTemplate.vue
@@ -4,9 +4,6 @@
 
 <template>
   <div class="main">
-    <h1>Some text</h1>
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus veritatis ducimus placeat, fugit molestias ratione et fuga magni id
-      quibusdam voluptatem. Cumque, aliquam! Maxime ullam assumenda doloribus cupiditate! Saepe, possimus?</p>
+    <p>#Vue + VitePWA starter pack</p>
   </div>
 </template>
